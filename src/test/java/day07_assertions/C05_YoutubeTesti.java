a2e3c187b2ec68a2e66e6f67db7087226b936ddd
ugur iyigunler <iyigunlerugur@gmail.com>
2022-05-30 18:02:15 +0300

    junit

---
+++ src/test/java/day07_assertions/C05_YoutubeTesti.java
@@ -0,0 +1,84 @@
+package day07_assertions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import static org.junit.Assert.*;
+
+public class C05_YoutubeTesti {
+    static WebDriver driver;
+
+    //1) Bir class oluşturun: YoutubeAssertions
+    //2) https://www.youtube.com adresine gidin
+    //3) Aşağıdaki adları kullanarak 4 test metodu oluşturun ve gerekli testleri yapin
+    //  ○ titleTest   => Sayfa başlığının “YouTube” oldugunu test edin
+    //  ○ imageTest   => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+    //   ○ Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+    //  ○ wrongTitleTest  => Sayfa basliginin “youtube” olmadigini dogrulayin
+    @BeforeClass
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+        // 2) https://www.bestbuy.com/ Adresine gidin farkli test method’lari olusturarak asagidaki testleri yapin
+        driver.get("https://www.youtube.com");
+
+
+    }
+
+
+    @Test
+    public void titleTest(){
+        //3) Aşağıdaki adları kullanarak 4 test metodu oluşturun ve gerekli testleri yapin
+        //  ○ titleTest   => Sayfa başlığının “YouTube” oldugunu test edin
+        String actualTitle=driver.getTitle();
+        String arananKelime="YouTube";
+        assertTrue(actualTitle.contains(arananKelime));
+
+    }
+
+
+    @Test
+    public void imageTest(){
+        //  ○ imageTest   => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+
+        WebElement resimElementi=driver.findElement(By.id("logo-icon"));
+Assert.assertTrue(resimElementi.isDisplayed());
+    }
+
+
+    @Test
+    public void searchBoxTest(){
+        //   ○ Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+
+        WebElement searchBoxElementi=driver.findElement(By.xpath("//input[@id='search']"));
+Assert.assertTrue(searchBoxElementi.isEnabled());
+
+    }
+
+
+    @Test
+    public void wrongTitleTest(){
+
+        //  ○ wrongTitleTest  => Sayfa basliginin “youtube” olmadigini dogrulayin
+        String actualTitle=driver.getTitle();
+        String istenmeyenKelime="youtube";
+        Assert.assertFalse(actualTitle.contains(istenmeyenKelime));
+
+    }
+
+
+    @After
+    public void teardown(){
+        driver.close();
+    }
+}
